b047456d0b2672b049d52722c230457c63b42906
marizaswart <mariza.swart@gmail.com>
2018-01-31 17:28:21 +0100

    Use factory bot to create a user with email and password, all user specs green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,10 @@ RSpec.describe User, type: :model do
     it {is_expected.to validate_presence_of(:email)}
     it {is_expected.to validate_confirmation_of(:password)}
 
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+
     context 'should not have an invalid email address' do
       emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com',
                 'asdf@example', 'ddd@.d. .d', 'ddd@.d']
