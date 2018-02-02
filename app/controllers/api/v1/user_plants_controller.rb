e988f3f965dde14c16085c6db0fd04847a26923c
marizaswart <mariza.swart@gmail.com>
2018-02-02 16:28:29 +0100

    Update api endpoint with more descriptive data about user and plants

--- app/controllers/api/v1/user_plants_controller.rb
+++ app/controllers/api/v1/user_plants_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::UserPlantsController < ApplicationController
 
   def create
-    @myplants = UserPlant.create(plant_id: params[:plant_id], user_id: params[:user_id])
+    myplant = UserPlant.create(plant_id: params[:plant_id], user_id: params[:user_id])
 
-    if @myplants.save
+    if myplant.save
       render json: { message: "Your plant has been added" }
     else
-      render json: { error: @myplants.errors.full_messages },
+      render json: { error: myplant.errors.full_messages },
              status: 400
     end
   end
 
   def index
-    @myplants = UserPlant.all
-    render json: @myplants
+    myplant = UserPlant.all
+    render json: myplant
   end
 end
--- app/serializers/user_plant_serializer.rb
+++ app/serializers/user_plant_serializer.rb
@@ -1,3 +1,14 @@
 class UserPlantSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :plant_id
+  attributes :plant
+  belongs_to :user
+
+  def plant
+   # object.plant.map do |plant|
+      PlantSerializer.new(object.plant).as_json
+   # end
+  end
+
+  def user
+    UserSerializer.new(object.user).as_json
+  end
 end
