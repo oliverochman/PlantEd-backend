14a99c36d250bcf57de8723ab384a24aab2ee654
oliver_ochman <oliver.ochman@hotmail.com>
2018-02-06 22:08:38 +0100

    [WiP] Update seed

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,6 +3,6 @@ class Plant < ApplicationRecord
   has_many :user_plants
   has_many :users, through: :user_plants
 
-  include :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: ["image/jpeg"]
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: ['image/jpeg']
 end
