61623b9534b4acd8a33e81475561506210be6957
oliver_ochman <oliver.ochman@hotmail.com>
2018-02-06 21:45:30 +0100

    Setup Amazon s3

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,7 @@ class Plant < ApplicationRecord
   validates :name, :description, presence: true
   has_many :user_plants
   has_many :users, through: :user_plants
+
+  include :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: ["image/jpeg"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206130646) do
+ActiveRecord::Schema.define(version: 20180206193952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 20180206130646) do
   create_table "plants", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "user_plants", force: :cascade do |t|
